fdb48e5eab56638e18d323cf646007303b4666a8
Justin Chadwell <jedevc@gmail.com>
2017-09-09 09:44:14 +0545

    Add basic filters and filter switching.

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 window.addEventListener('load', () => {
-	let booth = document.getElementById('booth')
 	let capture = document.getElementById('capture')
-	let camera = new Camera(document.getElementById('camera'))
+	let caption = document.getElementById('caption')
+
+	let videoCamera = document.getElementById('camera')
+	let videoPreview1 = document.getElementById('preview1').children[0]
+	let videoPreview2 = document.getElementById('preview2').children[0]
+
+	let camera = new Camera(videoCamera, videoPreview1, videoPreview2)
+	let filters = new Filters(videoCamera, videoPreview1, videoPreview2)
 
 	let overlay = document.getElementById('overlay')
 	overlay.style.display = 'none'
@@ -13,6 +19,7 @@ window.addEventListener('load', () => {
 		let img = document.createElement('img')
 		img.src = camera.takePicture()
 
+		// create thumbnail
 		let thumb = document.createElement('a')
 		thumb.classList.add('thumbnail')
 		thumb.href = img.src 
@@ -21,6 +28,7 @@ window.addEventListener('load', () => {
 
 		pictureCount++
 
+		// append thumbnail after flash complete
 		overlay.addEventListener('animationend', () => {
 			overlay.classList.remove('flash')
 			overlay.style.display = 'none'
@@ -28,15 +36,28 @@ window.addEventListener('load', () => {
 			roll.appendChild(thumb)
 			roll.scrollLeft = img.x
 		}, {once: true})
-
 		overlay.style.display = 'block'
 		overlay.classList.add('flash')
 	}
 
 	capture.addEventListener('click', takePicture)
-	window.addEventListener('keypress', (event) => {
+
+	videoPreview1.addEventListener('click', () => {
+		filters.update(-1)
+		caption.innerText = filters.current()
+	})
+
+	videoPreview2.addEventListener('click', () => {
+		filters.update(1)
+		caption.innerText = filters.current()
+	})
+
+	window.addEventListener('keyup', (event) => {
 		if (event.key == ' ' || event.key == 'Enter') {
 			takePicture()
+		} else if (event.key == 'Tab') {
+			filters.update(1)
+			caption.innerText = filters.current()
 		}
 	})
 
@@ -48,16 +69,24 @@ window.addEventListener('load', () => {
 })
 
 class Camera {
-	constructor(video=null) {
+	constructor(video=null, ...videos) {
 		this.video = video
 		if (this.video == null) {
 			this.video = document.createElement('video')
 		}
+		this.videos = videos
 
 		const constraints = {video: true}
+
+		// use video stream in all video elements
 		navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
 			this.video.srcObject = stream
 			this.video.play()
+
+			for (let vid of this.videos) {
+				vid.srcObject = stream
+				vid.play()
+			}
 		}).catch((err) => {
 			console.error(err.name, ':', err.message)
 		})
@@ -69,8 +98,61 @@ class Camera {
 		canvas.width = this.video.videoWidth
 		canvas.height = this.video.videoHeight
 
-		context.drawImage(this.video, 0, 0)
+		// if filter applied to main video, render image with filter
+		if (this.video.style.filter != null) {
+			context.filter = this.video.style.filter
+		}
 
+		context.drawImage(this.video, 0, 0)
 		return canvas.toDataURL()
+	}
+}
+
+function wrapAround(n, l) {
+	// wrap a number n into range (0, l)
+	if (n < 0) {
+		n = n % l
+		n += l
+	} else if (n >= l) {
+		n = n % l
+	}
+
+	return n
+}
+
+const filters = [
+	'',
+	'grayscale(100%)',
+	'sepia(100%)',
+	'brightness(50%)',
+	'brightness(200%)',
+	'invert(100%)'
+]
+
+class Filters {
+	constructor (main, left, right) {
+		this.main = main
+		this.left = left
+		this.right = right
+
+		this.filterNum = 0
+
+		this.update()
+	}
+
+	update (count=0) {
+		// update filters
+		this.filterNum = wrapAround(this.filterNum + count, filters.length)
+		let first = wrapAround(this.filterNum - 1, filters.length)
+		let second = wrapAround(this.filterNum + 1, filters.length)
+
+		// apply filters
+		this.main.style.filter = filters[this.filterNum]
+		this.left.style.filter = filters[first]
+		this.right.style.filter = filters[second]
+	}
+
+	current () {
+		return filters[this.filterNum]
 	}
 }
